3dba28877f05adc79d4391a0365e51e39e0059cd
12944qwerty <qwerty12944qwerty@gmail.com>
2024-05-24 00:39:31 -0500

    maybe root dir

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,6 @@ rm -r ./build/v5/pros-4
 mkdir ./build/v5/pros-4
 cp -r pros-doxygen-docs/* ./build/v5/pros-4/
 
-mkdir artifacts
 pushd build
-tar -zcf ../artifacts/pros-docs.tar.gz *
+tar -zcf ../pros-docs.tar.gz *
 popd
